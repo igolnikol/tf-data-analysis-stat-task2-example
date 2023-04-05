9de3c06ee6fa567443667fe5871cf4d59c70c7cc
igolnikol <129745179+igolnikol@users.noreply.github.com>
2023-04-05 21:51:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -12,6 +12,6 @@ def solution(p: float, x: np.array) -> tuple:
     # Не меняйте название функции и её аргументы
     alpha = 1 - p
     loc = 0.065
-    scale = 2 * x.mean() - 0.065
+    scale = 2 * x.mean() - 2*0.065
     return 2*x.mean() - 2*0.065 + 2 * np.sqrt(np.var(x)) * uniform.ppf(1 - alpha/2) / np.sqrt(len(x)), \
            2*x.mean() - 2*0.065 + 2 * np.sqrt(np.var(x)) * uniform.ppf(alpha/2) / np.sqrt(len(x))
